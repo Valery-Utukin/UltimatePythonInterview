67595553a14827e7e2df8e3930efd2a7543f66b6
Valery-Utukin <valeryvumvum@gmail.com>
2025-07-04 22:05:15 +0500

    Add response headers

--- Python Basics/http-test.py
+++ Python Basics/http-test.py
@@ -4,3 +4,6 @@ url = "https://google.com"
 
 response = requests.get(url)
 print(f"Status code: {response.status_code}")
+print("\nResponse headers:")
+for k, v in response.headers.items():
+    print(f"{k}: {v}")
